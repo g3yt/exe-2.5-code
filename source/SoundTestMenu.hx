4b2409712c8812b58120ab16635fea3495a7ae89
g3yt <83846251+g3yt@users.noreply.github.com>
2022-09-29 09:12:35 +0100

    mac fixes/maybe linux I havent tested

--- source/SoundTestMenu.hx
+++ source/SoundTestMenu.hx
@@ -58,7 +58,6 @@ class SoundTestMenu extends MusicBeatState
 			cam.bgColor.alpha = 0;
 			FlxCamera.defaultCameras = [cam];
 
-			DiscordClient.changePresence('In the Sound Test Menu', null);
 
 			new FlxTimer().start(0.1, function(tmr:FlxTimer)
 				{
